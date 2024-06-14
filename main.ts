aa6b0c808aea0fb03ec8fd0fba37a08cb483b3f8
jujusinhodoscapa <166841741+jujusinhodoscapa@users.noreply.github.com>
2024-06-14 13:37:09 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Angry)
+    music.play(music.stringPlayable("G A C5 C5 C5 A G G ", 120), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+	
+})
